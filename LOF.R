84baf3c193e95da0712528177720bec4a2361208
tomkru <kruty.tomas@gmail.com>
2018-04-25 22:25:43 +0200

    normalize input

--- LOF.R
+++ LOF.R
@@ -1,4 +1,6 @@
 lof.ensemble.mean <- function(listk, data, name = "") {
+  data <- read.csv(file = data, header = TRUE, sep = ",")
+  data <- as.data.frame(lapply(data[, -c(1, ncol(data))], normalize))
   outlier.scores <- vector(mode = "numeric", length = nrow(data))
   for(i in listk) {
     score <- lofactor(data, k=i)
@@ -9,4 +11,8 @@ lof.ensemble.mean <- function(listk, data, name = "") {
   outlier.score <- cbind.data.frame(outlier.score, data)
   write.table(outlier.score[,1], file = paste("LOF-dataresult-", name), append = FALSE, sep = ",", row.names = FALSE, col.names = FALSE)
   return(outlier.score)
+}
+
+normalize <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
 }
