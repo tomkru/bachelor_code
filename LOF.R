0a9288e4ca8d74d8868462b829339abda514b506
tomkru <kruty.tomas@gmail.com>
2018-04-25 21:12:13 +0200

    add writing output to file, ending in new 'name' parameter

--- LOF.R
+++ LOF.R
@@ -1,4 +1,4 @@
-lof.ensemble.mean <- function(listk, data) {
+lof.ensemble.mean <- function(listk, data, name = "") {
   outlier.scores <- vector(mode = "numeric", length = nrow(data))
   for(i in listk) {
     score <- lofactor(data, k=i)
@@ -7,5 +7,6 @@ lof.ensemble.mean <- function(listk, data) {
   outlier.scores$outlier.scores <- NULL
   outlier.score <- rowMeans(outlier.scores)
   outlier.score <- cbind.data.frame(outlier.score, data)
+  write.table(outlier.score[,1], file = paste("LOF-dataresult-", name), append = FALSE, sep = ",", row.names = FALSE, col.names = FALSE)
   return(outlier.score)
 }
