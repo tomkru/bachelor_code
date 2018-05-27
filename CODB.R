dde24a6f091d7c3488a74a151a9ff79bbca91407
tomkru <kruty.tomas@gmail.com>
2018-05-27 22:39:53 +0200

    eCODB

--- CODB.R
+++ CODB.R
@@ -39,7 +39,7 @@ normalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
 }
 
-Rank <- function(j, K, data, alpha, beta) {
+Rank <- function(j, K, data, minDev, maxDev, minKDist, maxKDist) {
   pcl <- 0
   deviation <- 0
   kdist <- 0
@@ -48,33 +48,53 @@ Rank <- function(j, K, data, alpha, beta) {
   deviation <- Deviation(j, data)
   kdist <- KDist(j, K, data)
   
-  cof <- K * pcl + (alpha/deviation) + (beta * kdist)
-  print(paste("K: ", K, "  pcl: ", pcl, "  deviation: ", deviation, "  kdist: ", kdist))
+  cof <- K * pcl - ((deviation - minDev) / (maxDev - minDev)) + ((kdist - minKDist) / (maxKDist - minKDist))
   return(cof)
 }
 
-CODB <- function(K, data, alpha, beta) {
-  dataNorm <- as.data.frame(lapply(data[, -c(1, ncol(data))], normalize))
-  dataNorm <- cbind.data.frame(name = data[, 1], dataNorm)
-  data <- cbind.data.frame(dataNorm, type = data[, ncol(data)])
+CODB <- function(K, data) {
   outliers <- data.frame()
+  
+  minDev <- Inf
+  minKDist <- Inf
+  maxDev <- -Inf
+  maxKDist <- -Inf
+  for (i in 1:nrow(data)) {
+    tempDev <- Deviation(data[i, -1], data)
+    tempKDist <- KDist(data[i, -1], K, data)
+    if(tempDev > maxDev) {
+      maxDev <- tempDev
+    }
+    if(tempDev < minDev) {
+      minDev <- tempDev
+    }
+    if(tempKDist > maxKDist) {
+      maxKDist <- tempKDist
+    }
+    if(tempKDist < minKDist) {
+      minKDist <- tempKDist
+    }
+  }
+  
   cof <- 0
   for (i in 1:nrow(data)) {
-    print(data[i, "name"])
-    cof <- Rank(data[i, -1], K, data, alpha, beta)
+    cof <- Rank(data[i, -1], K, data, minDev, maxDev, minKDist, maxKDist)
     outliers <- rbind.data.frame(outliers, cbind.data.frame(data[i,], cof))
   }
   return(outliers)
 }
 
-CODB.mean <- function(listK, data, alpha, beta) {
+CODB.mean <- function(listK, data, name = "") {
+  data <- read.csv(file = data, header = TRUE, sep = ",")
   outlier.scores <- vector(mode = "numeric", length = nrow(data))
   for(i in listK) {
-    score <- CODB(i, data, alpha, beta)
+    score <- CODB(i, data)
     outlier.scores <- cbind.data.frame(score$cof, outlier.scores)
   }
   outlier.scores$outlier.scores <- NULL
   outlier.score <- rowMeans(outlier.scores)
   outlier.score <- cbind.data.frame(outlier.score, data)
+  write.table(outlier.score, file = paste("CODB-dataresult-", name), append = FALSE, sep = ",", row.names = FALSE, col.names = FALSE)
   return(outlier.score)
 }
+
